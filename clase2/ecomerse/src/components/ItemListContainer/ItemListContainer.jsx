fd86fb947535cd7cb74cc538318934abc130510a
Adriel Joshua <adrieljoshua@hotmail.com>
2023-04-24 00:57:28 -0300

    Detalles

--- clase2/ecomerse/src/components/ItemListContainer/ItemListContainer.jsx
+++ clase2/ecomerse/src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,26 +1,31 @@
-import {useState,useEffect} from 'react'
+import { useState, useEffect } from 'react'
 
 import { ItemList } from '../ItemList/ItemList';
 import { getProductos } from '../../asyncmock';
+import { useParams } from 'react-router-dom';
 
 
- export const ItemListContainer = () => {
-    const [productos, setProductos] = useState([]);
+export const ItemListContainer = () => {
+  const [productos, setProductos] = useState([]);
+  const {categoria} = useParams()
+  console.log(categoria)
 
 
-    useEffect(()=>{
-        getProductos()
-        .then(response => setProductos(response))
-        .catch(error => console.error(error))
-    },[])
+  useEffect(() => {
+    getProductos()
+      .then(response => setProductos(response))
+      .catch(error => console.error(error))
+  }, [])
 
   return (
     <div>
-      <h2>Productos</h2>
-      <ItemList productos={productos} />
-     
-
+        <h2>Productos</h2>
+    
+    <ItemList productos={productos} />
     </div>
+    
+
+    
   )
 }
 
--- clase2/ecomerse/src/components/Menu/NavBar.jsx
+++ clase2/ecomerse/src/components/Menu/NavBar.jsx
@@ -3,39 +3,46 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import CartWidget from '../CartWidget/CartWidget';
+import { Link, NavLink} from 'react-router-dom';
+
 
 const NavBar = () => {
-    return (
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">Mi Tienda </Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
-              <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Actis</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-            <Nav>
-              <Nav.Link eventKey={2} href="#memes">
-                        <CartWidget />          
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-     
-    )
-  }
-  
+ 
+
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Link to="/">
+          Mi Tienda
+        </Link>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <NavLink to="/categoria/cuadros-minimalistas">Minimalistas</NavLink>
+             <NavLink to="/categoria/cuadros-flores"> Flores</NavLink>
+             
+            <NavDropdown title="Categorias" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Actis</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                12
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">123</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Link to="/cart" >
+              <CartWidget />
+            </Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+  )
+}
+
 export default NavBar
