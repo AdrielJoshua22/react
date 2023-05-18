d97cc793e7fe1ab0c590c4d777ade070adc132f6
Adriel Joshua <adrieljoshua@hotmail.com>
2023-05-18 10:16:37 -0300

    agregamos Loading

--- clase2/ecomerse/src/components/ItemList/ItemList.jsx
+++ clase2/ecomerse/src/components/ItemList/ItemList.jsx
@@ -1,13 +1,16 @@
+import { memo } from "react"
 import { Item } from "../Item/Item"
 import "./ItemList.css"
 
-export const ItemList = ({productos}) => {
-  return (
-    <div className="contenedorProductos">
-        {productos.map(prod => <Item key={prod.id} {...prod} />)}
-      
-    </div>
-  )
-}
+export const ItemList = memo(({productos}) => {
+    return (
+      <div className="contenedorProductos">
+          {productos.map(prod => <Item key={prod.id} {...prod} />)}
+        
+      </div>
+    )
+  }
+  
+)
 
  
