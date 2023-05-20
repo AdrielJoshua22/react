86211d71fcc8d507fce8a1835c528fa731929ab5
Adriel Joshua <adrieljoshua@hotmail.com>
2023-05-20 16:28:59 -0300

    agregaos sumar total

--- clase2/ecomerse/src/components/CartContainer/CartContainer.jsx
+++ clase2/ecomerse/src/components/CartContainer/CartContainer.jsx
@@ -1,24 +1,24 @@
 import { addDoc, collection, getFirestore } from "firebase/firestore"
-import { useCartContext } from "../../context/CartContext"
+import { useCartContext , precioTotal } from "../../context/CartContext"
 import { useState } from "react"
 
 
 export const CartContainer = () => {
-    const { cartList/* , precioTotal, vaciarCarrito, */ } = useCartContext()
-    const { dataForm, setDataForm } = useState({
+       const [ cartList  , precioTotal, /* vaciarCarrito,  */ ] = useCartContext()
+       const [dataForm , setDataForm] = useState({
         nombre:'',
         telefono:'',
         email: ''
-
-    })
-
+        
+      })
+    
     const generarCompra = (evt) => {
         evt.preventDefault()
         const order = {}
         order.comprador = dataForm /* { nombre: "", telefono: "", email: "" } */
-        order.items = cartList.map(({ nombre, id, precio }) => ({ id, nombre, precio }))
-        /*  order.total = precioTotal()
-         */
+        order.items = cartList.map(({ nombre, id, precio,cantidad }) => ({ id, nombre, precio,cantidad }))
+         order.total = precioTotal()
+        
         const dbFirestore = getFirestore()
         const orderCollection = collection(dbFirestore, 'ordenes')
 
@@ -42,28 +42,29 @@ console.log(dataForm)
             {cartList.map(productos => (
                 <div className="w-50">
                     <img className="w-25" src={productos.img} alt="imagen" />
-                    <label> Precio {productos.precio}  </label>
+                    <label> Precio {productos.precio} -  Cantidad : {productos.cantidad} </label>
                     <button > X </button>
                 </div>
             ))}
+            <h3>Precio total de la compra :{precioTotal()} </h3>
             {/*  <button onClick={vaciarCarrito} className="btn- btn-outline-danger">vaciar Carrito</button>  */}
-           <button onClick={generarCompra} className="btn- btn-outline-danger">Generar Compra</button> 
+           {/* <button onClick={generarCompra} className="btn- btn-outline-danger">Generar Compra</button>  */}
             <form onSubmit={generarCompra}>
                 <input
-                    type="text"
-                    name="nombre"
+                    type='text'
+                    name='nombre'
                     onChange={handleOnChange}
                     value={dataForm.nombre}
                     placeholder="ingrese el nombre" />
                 <input
-                    type="text"
-                    name="telefono"
+                    type='text'
+                    name='telefono'
                     onChange={handleOnChange}
                     value={dataForm.telefono}
                     placeholder="ingrese el telefono" />
                 <input
-                    type="text"
-                    name="email"
+                    type='text'
+                    name='email'
                     onChange={handleOnChange}
                     value={dataForm.email}
                     placeholder="ingrese el email" />
--- clase2/ecomerse/src/context/CartContext.jsx
+++ clase2/ecomerse/src/context/CartContext.jsx
@@ -1,29 +1,43 @@
 import { createContext, useState, useContext } from "react"
 
 
- const CartContext = createContext([])
+const CartContext = createContext([])
 export const useCartContext = () => useContext(CartContext)
 
 
-export const CartContextProvider = ({children})=> {
+export const CartContextProvider = ({ children }) => {
 
     const [cartList, setCartList] = useState([])
 
-    const addToCart = (newProduct)=>{
+    const addToCart = (newProduct) => {
 
-        setCartList([
-            ...cartList,
-            newProduct
-        ])
+        const item1 = cartList.findIndex(prod => newProduct.id === prod.id)
+
+        if (item1 === -1) {
+            setCartList([
+                ...cartList,
+                newProduct
+            ])
+
+        } else {
+            cartList[item1].cantidad = newProduct.cantidad
+            setCartList([...cartList])
+        }
     }
+
+    const precioTotal = () =>{
+        cartList.reduce((total, objeProducto) => total = (objeProducto.cantidad * objeProducto.precio), 0)
+    }
+        
 
     return (
         <CartContext.Provider value={{
             cartList,
-            addToCart
+            addToCart,
+            precioTotal
         }}>
 
-          
+
             {children}
 
         </CartContext.Provider>
