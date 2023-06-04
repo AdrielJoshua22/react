b19c9e1ef8065dfed5f5875458ef60dd0208934f
Adriel Joshua <adrieljoshua@hotmail.com>
2023-06-04 19:49:19 -0300

    listo!

--- clase2/ecomerse/src/components/ItemDetail/ItemDetail.jsx
+++ clase2/ecomerse/src/components/ItemDetail/ItemDetail.jsx
@@ -1,4 +1,4 @@
- import React, { useState, useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { CartContext } from '../../context/CartContext';
 import { Contador } from '../Contador/Contador';
@@ -14,30 +14,30 @@ import './ItemDetail.css';
   };
 
   return (
-    <div className="item-detail-container">
-      <div className="item-detail-info">
-        <h2 className="item-detail-title">NOMBRE: {nombre}</h2>
-        <h3 className="item-detail-price">Precio: {precio}</h3>
-        <h3 className="item-detail-size">Tamaño: {tamaño}</h3>
-        <p className="item-detail-description">
+    <div className="card">
+      <div className="card-body">
+        <h2 className="card-title">NOMBRE: {nombre}</h2>
+        <h3 className="card-price">Precio: {precio}</h3>
+        <h3 className="card-size">Tamaño: {tamaño}</h3>
+        <p className="card-description">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa earum recusandae maxime perferendis dolorem suscipit magnam illo vitae, sunt natus?
         </p>
-        <img className="item-detail-image" src={img} alt="imagen" />
-      </div>
+        <img className="card-image" src={img} alt="imagen" />
 
-      {!isCant ? (
-        <Contador stock={10} inicial={1} onAdd={onAdd} />
-      ) : (
-        <div className="item-detail-buttons">
-          <Link to="/cart" className="btn btn-outline-danger">
-            Terminar compra
-          </Link>
-          <Link to="/" className="btn btn-outline-success">
-            Seguir la compra
-          </Link>
-        </div>
-      )}
+        {!isCant ? (
+          <Contador stock={10} inicial={1} onAdd={onAdd} />
+        ) : (
+          <div className="card-buttons">
+            <Link to="/cart" className="btn btn-outline-danger">
+              Terminar compra
+            </Link>
+            <Link to="/" className="btn btn-outline-success">
+              Seguir la compra
+            </Link>
+          </div>
+        )}
+      </div>
     </div>
   );
-}; 
+};
 
